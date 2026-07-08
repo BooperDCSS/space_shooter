53224c6e9eee6c7dadfb46b1b022238abce2e09b
booper <otosound@protonmail.com>
2026-07-08 15:25:27 -0400

    final messy implementation of basic features; next step will introduce classes

--- code/main.py
+++ code/main.py
@@ -23,8 +23,8 @@ def main():
     ### IMPORTS ###
     player_surf = pygame.image.load(ROOT_DIR.joinpath("images", "player.png")).convert_alpha()
     player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-    player_direction = pygame.math.Vector2(2, -1)
-    player_speed = 100
+    player_direction = pygame.math.Vector2()
+    player_speed = 300
     ## the vector defaults to 0,0 if you don't provide x,y arguments 
     
     star_surf = pygame.image.load(ROOT_DIR.joinpath("images", "star.png")).convert_alpha()
@@ -37,8 +37,9 @@ def main():
     laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
     while running:
+        dt = clock.tick(60) / 1000
+
         # this is my event loop
-        dt = clock.tick(30) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -48,18 +49,30 @@ def main():
             display_surface.blit(star_surf, pos)
         # blit stands for block image transfer
 
+        keys = pygame.key.get_pressed()
+        player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a]) # resolves to 1 or -1
+        player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w]) # resolves to 1 or -1
+        player_direction = player_direction.normalize() if player_direction else player_direction
+        player_rect.center += player_direction * player_speed * dt
+
+        if pygame.key.get_just_pressed()[pygame.K_SPACE]:
+            print("FIRE ZEE MISSILES!")
+        
+        if keys[pygame.K_LSUPER] and keys[pygame.K_q]:
+            running= False
+
         display_surface.blit(laser_surf, laser_rect)
         display_surface.blit(meteor_surf, meteor_rect)
 
-        if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-            player_direction.x *= -1
-        if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
-            player_direction.y *= -1
+        # if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        #     player_direction.x *= -1
+        # if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
+        #     player_direction.y *= -1
         # the bouncing effect relies on just the x or y value modifying
         # if you modify both, it just bounces back and forth along a single trajectory
         # but modifying just the x or y changes the direction of the vector entirely
 
-        player_rect.center += player_direction * player_speed * dt
+
         display_surface.blit(player_surf, player_rect)
         
         pygame.display.update()
