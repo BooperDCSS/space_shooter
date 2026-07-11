82f511c986abac009ca9e7c0584d7859af433983
booper <otosound@protonmail.com>
2026-07-11 17:57:01 -0400

    added animated explosions for every meteor destroyed

--- code/main.py
+++ code/main.py
@@ -5,12 +5,16 @@ from random import randint, uniform
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 CODE_DIR = Path(__file__).parent.resolve()
 ROOT_DIR = Path(CODE_DIR).parent.resolve()
+EXPLOSIONS_DIR = Path(ROOT_DIR).joinpath("images", "explosion")
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
-        self.image = pygame.image.load(ROOT_DIR.joinpath("images", "player.png")).convert_alpha()
-        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.image = pygame.image.load(
+            ROOT_DIR.joinpath("images", "player.png")
+        ).convert_alpha()
+        self.rect = self.image.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
         self.groups = groups
 
         self.direction = pygame.math.Vector2()
@@ -19,21 +23,27 @@ class Player(pygame.sprite.Sprite):
         # laser and cooldown attributes
         self.can_shoot = True
         self.laser_shoot_time = 0
-        self.cooldown_duration = 400 # ms
+        self.cooldown_duration = 400  # ms
 
     def laser_timer(self):
         if not self.can_shoot:
-            current_time = pygame.time.get_ticks() # returns value in ms
+            current_time = pygame.time.get_ticks()  # returns value in ms
             if current_time - self.laser_shoot_time >= self.cooldown_duration:
                 self.can_shoot = True
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        #movement
-        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a]) # resolves to 1 or -1
-        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w]) # resolves to 1 or -1
-        self.direction = self.direction.normalize() if self.direction else self.direction
+        # movement
+        self.direction.x = int(keys[pygame.K_d]) - int(
+            keys[pygame.K_a]
+        )  # resolves to 1 or -1
+        self.direction.y = int(keys[pygame.K_s]) - int(
+            keys[pygame.K_w]
+        )  # resolves to 1 or -1
+        self.direction = (
+            self.direction.normalize() if self.direction else self.direction
+        )
         self.rect.center += self.direction * self.speed * dt
 
         # zee missiles!
@@ -42,57 +52,105 @@ class Player(pygame.sprite.Sprite):
             self.can_shoot = False
             self.laser_shoot_time = pygame.time.get_ticks()
 
-        self.laser_timer() # every update, run the laser_timer method
+        self.laser_timer()  # every update, run the laser_timer method
+
 
 class Stars(pygame.sprite.Sprite):
     def __init__(self, groups, surface):
         super().__init__(groups)
         self.image = surface
-        self.rect = self.image.get_frect(center = (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
+        self.rect = self.image.get_frect(
+            center=(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT))
+        )
+
 
 class Laser(pygame.sprite.Sprite):
+    # remember that Laser gets called inside the Player class missiles section
     def __init__(self, surface, pos, groups):
         super().__init__(groups)
         self.image = surface
-        self.rect = self.image.get_frect(midbottom = pos)
+        self.rect = self.image.get_frect(midbottom=pos)
 
     def update(self, dt):
         self.rect.centery -= 400 * dt
         if self.rect.bottom < 0:
             self.kill()
 
+
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, surface, groups):
         super().__init__(groups)
-        self.image = surface
-        self.rect = self.image.get_frect(midbottom = (randint(0, WINDOW_WIDTH), -1))
-        self.start_time = pygame.time.get_ticks() # this triggers on creation
+        self.original = surface
+        self.image = self.original
+        self.rect = self.image.get_frect(midbottom=(randint(0, WINDOW_WIDTH), -1))
+        self.start_time = pygame.time.get_ticks()  # this triggers on creation
 
-        self.direction = pygame.math.Vector2(uniform(-0.5, 0.5), 1)
         self.speed = randint(200, 400)
+
+        # rotation values
+        self.direction = pygame.math.Vector2(uniform(-0.5, 0.5), 1)
+        self.rotation = randint(-30, 30)
 
     def update(self, dt):
         self.rect.center += self.direction * self.speed * dt
         if pygame.time.get_ticks() - self.start_time >= 4000:
             self.kill()
 
-def laser_collisions():
+        if self.rotation > 0:
+            self.rotation += randint(0, 100) * dt
+        else:
+            self.rotation -= randint(-100, 0) * dt
+        self.image = pygame.transform.rotozoom(self.original, self.rotation, 1)
+        self.rect = self.image.get_frect(center=self.rect.center)
+
+
+class Explosions(pygame.sprite.Sprite):
+    def __init__(self, frames, pos, groups):
+        super().__init__(groups)
+        self.frames = frames
+        self.frame_index = 0
+
+        self.image = frames[self.frame_index]
+        self.rect = self.image.get_frect(center=pos)
+
+    def update(self, dt):
+        self.frame_index += 20 * dt
+        # dt is always fractional... so the frame_index increments slowly
+        # the 20 functions as a kind of speed control on the animation
+
+        if self.frame_index < len(self.frames):
+            self.image = self.frames[int(self.frame_index)]
+        else:
+            self.kill()
+
+
+### GAME FUNCTIONS USED IN MAIN() ###
+def check_laser_meteor_collisions():
     for laser in laser_sprites:
-        laser_hits = pygame.sprite.spritecollide(laser, meteor_sprites, True, pygame.sprite.collide_mask)
+        laser_hits = pygame.sprite.spritecollide(
+            laser, meteor_sprites, True, pygame.sprite.collide_mask
+        )
+        # note that this function removes the meteor on collision too
         if laser_hits:
+            Explosions(explosion_frames, laser.rect.midtop, all_sprites)
             laser.kill()
+
 
 def display_score(font):
     current_time = pygame.time.get_ticks() // 100
     text_surface = font.render(str(current_time), True, "#eeeeee")
-    text_rect = text_surface.get_frect(midbottom = ((WINDOW_WIDTH / 2), (WINDOW_HEIGHT - 50)))
+    text_rect = text_surface.get_frect(
+        midbottom=((WINDOW_WIDTH / 2), (WINDOW_HEIGHT - 50))
+    )
     display_surface.blit(text_surface, text_rect)
     padded_rect = text_rect.inflate(20, 15).move(0, -5)
     pygame.draw.rect(display_surface, "red", padded_rect, 5, 5)
 
+
+
 ### CREATING GAME WINDOW AND TITLE (notice set_mode takes a tuple) ###
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Shooting... in space!!")
+pygame.display.set_caption("Asteroids from the Sky!!")
 
 ### CREATING GROUPS AND SPRITES ###
 all_sprites = pygame.sprite.Group()
@@ -100,13 +158,37 @@ meteor_sprites = pygame.sprite.Group()
 laser_sprites = pygame.sprite.Group()
 
 ### IMPORT SURFACES OUTSIDE CLASSES WHEN USED IN LOOP (only loads once) ###
-star_surface = pygame.image.load(ROOT_DIR.joinpath("images", "star.png")).convert_alpha()
-laser_surface = pygame.image.load(ROOT_DIR.joinpath("images", "laser.png")).convert_alpha()
-meteor_surface = pygame.image.load(ROOT_DIR.joinpath("images", "meteor.png")).convert_alpha()
+star_surface = pygame.image.load(
+    ROOT_DIR.joinpath("images", "star.png")
+).convert_alpha()
+
+laser_surface = pygame.image.load(
+    ROOT_DIR.joinpath("images", "laser.png")
+).convert_alpha()
+
+meteor_surface = pygame.image.load(
+    ROOT_DIR.joinpath("images", "meteor.png")
+).convert_alpha()
+
+# this is a list of all the surfaces in the explosion animation
+# this works because the animation is numbered sequentially, 0 - 20
+# the lesson is that you can load Surfaces into a list
+explosion_frames = [
+    pygame.image.load(ROOT_DIR.joinpath(EXPLOSIONS_DIR, f"{i}.png")).convert_alpha()
+    for i in range(21)
+]
+
+
+# def get_stem(file):
+#     return int(file.stem)
+# explosion_frames = sorted([ex_frame for ex_frame in EXPLOSIONS_DIR.iterdir() if ex_frame.is_file()], key=get_stem)
+# this is my old solution for getting the frames; it works but requires loading the surface within the class
+
 
 
 ### CUSTOM EVENTS DEFINITIONS ###
 meteor_event = pygame.event.custom_type()
+
 
 def main():
     pygame.init()
@@ -142,11 +224,15 @@ def main():
         # update the sprites according to their update methods
         # and check for collisions
         all_sprites.update(dt)
-        laser_collisions()
+        check_laser_meteor_collisions()
 
         # check for game-ending collision
-        if pygame.sprite.spritecollide(player, meteor_sprites, False, pygame.sprite.collide_mask):
-            print("Game over, man")
+        if pygame.sprite.spritecollide(
+            player, meteor_sprites, False, pygame.sprite.collide_mask
+        ):
+            print(
+                f"Game over, man. You scored {pygame.time.get_ticks() // 100} points. Nice job!"
+            )
             running = False
 
         # this is how we draw the game; after everything happens, draw and update!
@@ -158,6 +244,7 @@ def main():
         pygame.display.update()
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
